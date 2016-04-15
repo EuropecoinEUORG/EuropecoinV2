14b6ed53cfe3948e41aa1c083d089431c2dd8224
Christian Knöpke <apfelbaum@email.de>
2016-04-15 13:23:44 +0200

    Add Debug Block

--- src/main.h
+++ src/main.h
@@ -28,6 +28,7 @@ class CNode;
 
 static const int LAST_POW_BLOCK = 20160;
 static const int FORK_BLOCK = 820000;
+static const int DEBUG_BLOCK = 930000;
 static const int LAST_BLOCK_BONUS_RATE = 850240; // ~3 weeks after fork
 
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
