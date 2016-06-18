157c55627b6a4495490f4fca10af48133ddd8f02
Christian Knöpke <apfelbaum@email.de>
2016-06-18 23:47:25 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1002,16 +1002,15 @@ int64_t GetProofOfWorkReward(int64_t nFees)
 int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
 {
     int64_t nSubsidy = 0;
+    int64_t nRewardCoinYear = 0;
+    int64_t MAX_MINT_PROOF_OF_STAKE = 1 * CENT;
+    int DAILY_BLOCKCOUNT = 1440;
 
     if(pindexBest->nHeight <= FORK_BLOCK) {
 
         // old dev's attempt of create a variable PoS....
         // code enforced until block 820.000
-        int64_t nRewardCoinYear = 0;
-        int64_t MAX_MINT_PROOF_OF_STAKE = 1 * CENT;
-        int DAILY_BLOCKCOUNT = 1440;
-
-        if(pindexBest->nHeight < 7 * DAILY_BLOCKCOUNT)
+         if(pindexBest->nHeight < 7 * DAILY_BLOCKCOUNT)
             nRewardCoinYear = 2.5 * MAX_MINT_PROOF_OF_STAKE;
         else if(pindexBest->nHeight < 14 * DAILY_BLOCKCOUNT)
             nRewardCoinYear = 10 * MAX_MINT_PROOF_OF_STAKE;
@@ -1033,12 +1032,7 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
 
         // old dev's attempt of create a variable PoS....
         // code enforced until block 820.000
-        int64_t nRewardCoinYear = 0;
-        int64_t MAX_MINT_PROOF_OF_STAKE = 1 * CENT;
-        int DAILY_BLOCKCOUNT = 1440;
-
 	nRewardCoinYear = 8 * MAX_MINT_PROOF_OF_STAKE;
-
         // hard to understand why he did this....
         if(pindexBest->nHeight > 21 * DAILY_BLOCKCOUNT)
             nSubsidy = nCoinAge  / COIN * nRewardCoinYear / 365;
