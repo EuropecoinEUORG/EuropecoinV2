55e8ff89ee07ddf0997574d18382e36318c2bff8
Christian Knöpke <apfelbaum@email.de>
2016-06-18 22:53:24 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,10 @@ CBigNum bnProofOfWorkFirstBlock(~uint256(0) >> 30);
 //Changes in Blocktime
 int nHeight_FORK_BLOCK2 = pindexBest->nHeight;
 unsigned int nTargetSpacing = 1 * 60;
-if(nHeight_FORK_BLOCK2 > FORK_BLOCK2){nTargetSpacing = 5 * 60;}
+if(nHeight_FORK_BLOCK2 > FORK_BLOCK2)
+{ 
+	nTargetSpacing = 5 * 60;
+}
 unsigned int nRetarget = 1;
 unsigned int nStakeMinAge = 72 * 60 * 60; // 3 days
 unsigned int nStakeMaxAge = -1;           //unlimited
